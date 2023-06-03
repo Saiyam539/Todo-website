1f7092e9baa1645e1663855a5b7b8be5dfe589d4
Saiyam Arora <77787626+Saiyam539@users.noreply.github.com>
2023-06-04 01:32:55 +0530

    Update settings.py

--- Todo/settings.py
+++ Todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-j#!2s4w@ak312#ko+fpz%=4k97+1+v%aa0vv-u1a)7h5iqihgj
 DEBUG = False
 
 
-ALLOWED_HOSTS = ["todo-website-production.up.railway.app","https://todo-website-production.up.railway.app/]
+ALLOWED_HOSTS = ["todo-website-production.up.railway.app","https://todo-website-production.up.railway.app/"]
 
 # Application definition
 
