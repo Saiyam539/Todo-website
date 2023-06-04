e6ac8df41259beb90ffa93c81bbd211b9b4096e3
Saiyam Arora <77787626+Saiyam539@users.noreply.github.com>
2023-06-04 14:10:28 +0530

    Update settings.py

--- Todo/settings.py
+++ Todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-j#!2s4w@ak312#ko+fpz%=4k97+1+v%aa0vv-u1a)7h5iqihgj
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["todo-website-production.up.railway.app"]
+ALLOWED_HOSTS = ['.vercel.app','now.sh']
 
 # Application definition
 
